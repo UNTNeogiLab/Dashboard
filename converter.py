713d972f59de3d09bef3f453cc6983487479c026
bageljr <aryanagarwal@my.unt.edu>
2021-06-18 22:08:08 -0500

    Do it

--- converter.py
+++ converter.py
@@ -1,19 +1,30 @@
 import os
 
-import visualizer.utils
+import zarr
 from pathlib import Path
 import xarray as xr
 from dask.distributed import Client
+
+from visualizer import utils
+
+compressor = zarr.Blosc(cname="zstd", clevel=3, shuffle=2)
 if __name__ == '__main__':
-    #client = Client()
+    client = Client()
     for file in list(Path(".").rglob("*.zarr")):
         print(file)
         ds1 = xr.open_dataset(file,engine="zarr")
         if not "data_type" in ds1.attrs:
             ds1.attrs["data_type"] = "RASHG"
             ds1.to_zarr(file,mode="w",compute=True)
-    '''
-    extensions = {"5nc": fiveD.grapher}
-    for file in getDir(extensions):
-        fiveD.grapher(file,client).to_zarr()
-        '''
+    for file in list(Path(".").rglob("*.5nc")):
+        filename = str(file).replace(f".{utils.extension(file)}", '.zarr')
+        if not filename in list(Path(".").rglob("*.zarr")):
+            ds = xr.open_dataarray(file,engine="zarr")
+            coords = ds.coords
+            ds_coords = ds.assign_coords(power=0).expand_dims("power")
+            data = xr.Dataset(data_vars={"ds1": ds_coords},
+                              attrs=ds.attrs,
+                              coords=coords)
+            data["data_type"] = "RASHG"
+            print(data)
+            data.to_zarr(filename, encoding={"ds1": {"compressor": compressor}}, consolidated=True,compute=True)
