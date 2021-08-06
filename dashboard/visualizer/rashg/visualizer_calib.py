bfd162d0d63699abd0c231518d7feb253cd038de
bageljr <aryanagarwal897@gmail.com>
2021-08-06 14:46:55 -0500

    Allow user to interpolate against arbitrary powers

--- dashboard/visualizer/rashg/visualizer_calib.py
+++ dashboard/visualizer/rashg/visualizer_calib.py
@@ -2,6 +2,8 @@
 Module for viewing calibration files
 """
 from pathlib import Path, PosixPath
+
+import numpy as np
 import param
 import xarray as xr
 import holoviews as hv
@@ -18,16 +20,25 @@ class Grapher(param.Parameterized):
     """visualizer for calibration files"""
     dataset = param.ObjectSelector(objects=["Pwr", "Pwrstd", "Vol", "Volstd"], default="Pwr")
     wavelength = param.Selector()
+    pstart = param.Integer(default=0)
+    pstop = param.Integer(default=100)
+    pstep = param.Number(default=5)
 
     def _update_dataset(self):
         """
         Opens selected dataset
         """
-        self.pc_reverse = utils.interpolate(PosixPath(self.filename))
-        self.pol_dim = hv.Dimension('Polarization', soft_range=(0, 10), unit="degrees")
+        self.interpolate()
         self.data = xr.open_zarr(self.filename)
         self.param['wavelength'].objects = self.data["Pwr"].coords['wavelength'].values.tolist()
         self.wavelength = self.data["Pwr"].coords["wavelength"].min().values
+
+    @param.depends("pstart", "pstop", "pstep", watch=True)
+    def interpolate(self):
+        power = np.arange(self.pstart, self.pstop, self.pstep)
+        self.pc_reverse = utils.interpolate(PosixPath(self.filename), pwr=power)
+        self.pol_dim = hv.Dimension('Polarization', soft_range=(0, 10), unit="degrees")
+        self.pwr_dim = hv.Dimension("Power", range=(self.pstart, self.pstop))
 
     def __init__(self, filename, client_input):
         """
@@ -50,13 +61,13 @@ class Grapher(param.Parameterized):
                               tools=['hover'], framewise=True)]
         return hv.Curve(output).opts(opts)
 
-    @param.depends("wavelength")
+    @param.depends("wavelength", "pstart", "pstop", "pstep")
     def reverse_nav(self):
         """Reversed navigation graph"""
         output = self.pc_reverse.sel(wavelength=self.wavelength)
         opts = [hv.opts.Curve(title=f"Wavelength: {self.wavelength}",
                               tools=['hover'], framewise=True)]
-        return hv.Curve(output, "power", "polarization").opts(opts).redim(polarization=self.pol_dim)
+        return hv.Curve(output, "power", "polarization").opts(opts).redim(polarization=self.pol_dim, power=self.pwr_dim)
 
     def view(self):
         """
